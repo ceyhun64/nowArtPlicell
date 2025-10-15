9f7e65cbac382a5c5c34c65108b5eacb3da5045a
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-10-15 16:35:28 +0300

    7. commit

--- lib/session.ts
+++
@@ -1,9 +0,0 @@
-import { withSession } from "next-session";
-
-export const session = withSession({
-  cookie: {
-    secure: process.env.NODE_ENV === "production",
-    httpOnly: true,
-    maxAge: 60 * 60 * 24, // 1 gün
-  },
-});
