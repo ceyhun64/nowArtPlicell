06cbfff3b3000c70d02d75f2e4004da617e9b72c
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-10-22 14:06:36 +0300

    26. commit

--- app/admin/blogs/page.tsx
+++ app/admin/blogs/page.tsx
@@ -1,15 +1,12 @@
-"use client"
-import dynamic from "next/dynamic";
+// app/admin/dashboard/page.tsx
+import React from "react";
+import AdminBlogs from "@/components/admin/blogs/blogs";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
-// Client component'i dynamic import ile yükle, ssr kapalı
-const AdminBlogs = dynamic(() => import("@/components/admin/blogs/blogs"), {
-  ssr: false,
-});
-
 export default async function AdminBlogsPage() {
+  // Server-side session kontrolü
   const session = await getServerSession(authOptions);
 
   // Eğer giriş yoksa veya role ADMIN değilse login sayfasına yönlendir
@@ -17,6 +14,9 @@ export default async function AdminBlogsPage() {
     redirect("/admin");
   }
 
-  return <AdminBlogs />;
+  return (
+    <div>
+      <AdminBlogs />
+    </div>
+  );
 }
- 
