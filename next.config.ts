0f8b1dec262981534a2e571c50ebf446be4dc9f0
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-10-23 18:32:55 +0300

    43. commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* Mevcut diğer config seçenekleriniz varsa buraya bırakın */
   images: {
-    domains: ["res.cloudinary.com"], // Bu satırı ekleyin
+    domains: ["res.cloudinary.com"], // Mevcut ayar
   },
 };
 
